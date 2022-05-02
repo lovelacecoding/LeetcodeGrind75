94b2027fd06bd1a88f67088c15ef587e896767ae
louelladev <louellacreemers9@gmail.com>
2022-05-02 16:19:37 +0200

    adding valid palindrome

---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ValidPalindrome;
+
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        Program pr = new Program();
+        pr.Start();
+    }
+
+    public void Start()
+    {
+        Console.WriteLine(IsPalindrome(" "));   
+    }
+    
+    public bool IsPalindrome(string s)
+    {
+        var lower = s.ToLower().Replace(" ", "");
+        var lowerRegex = Regex.Replace(lower, "[^a-zA-Z0-9]", "");
+        /*char[] lowerArray = lowerRegex.ToCharArray();
+        char[] lowerArrayR = lowerRegex.Reverse().ToArray();
+
+        for (int i = 0; i < lowerArray.Length; i++)
+        {
+            Console.WriteLine($"{lowerArray[i]} rev: {lowerArrayR[i]}");
+            if (lowerArray[i] != lowerArrayR[i])
+            {
+                return false;
+            }
+        }
+        return true;*/
+
+        return lowerRegex.SequenceEqual(lowerRegex.Reverse());
+    }
+}
